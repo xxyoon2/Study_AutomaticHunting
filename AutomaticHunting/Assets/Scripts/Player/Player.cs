a2209a0d5a282a9ecc06d4c9abfeb2734e3a1b96
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-12-27 04:07:56 +0900

    Feat: 플레이어 공격 구현
    - 정해진 시간이 지나면 자동으로 공격하도록 구현

--- AutomaticHunting/Assets/Scripts/Player/Player.cs
+++ AutomaticHunting/Assets/Scripts/Player/Player.cs
@@ -7,9 +7,22 @@ public class Player : MonoBehaviour
     [SerializeField] private int _hp = 10;
     [SerializeField] private int _speed;
 
-    private void Attack()
-    {
+    private IEnumerator _attack = null;
+    private WaitForSeconds _actionTime = new WaitForSeconds(3.0f);
 
+    private void Start()
+    {
+        _attack = Attack();
+        StartCoroutine(_attack);
+    }
+
+    IEnumerator Attack()
+    {
+        while(true)
+        {
+            yield return _actionTime;
+            Debug.Log("죽어랏!!!!~!~!");
+        }
     }
 
     private void Hit()
